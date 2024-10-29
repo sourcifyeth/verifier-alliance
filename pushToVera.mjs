2b6fc0668041f16ab564fce3236e5d0fc95b64e5
Marco Castignoli <marco@castignoli.it>
2024-10-29 08:27:13 +0100

    add try catch

--- pushToVera.mjs
+++ pushToVera.mjs
@@ -113,274 +113,275 @@ async function upsertAndGetId(
       verifiedContractCount = rowCount;
 
       for (const contract of verifiedContracts) {
-        const {
-          id: verifiedContractId,
-          deployment_id,
-          compilation_id,
-        } = contract;
+        try {
+          const {
+            id: verifiedContractId,
+            deployment_id,
+            compilation_id,
+          } = contract;
 
-        // Update the CURRENT_VERIFIED_CONTRACT counter
-        CURRENT_VERIFIED_CONTRACT = verifiedContractId;
-        fs.writeFileSync(
-          COUNTER_FILE,
-          CURRENT_VERIFIED_CONTRACT.toString(),
-          "utf8"
-        );
+          // Update the CURRENT_VERIFIED_CONTRACT counter
+          CURRENT_VERIFIED_CONTRACT = verifiedContractId;
+          fs.writeFileSync(
+            COUNTER_FILE,
+            CURRENT_VERIFIED_CONTRACT.toString(),
+            "utf8"
+          );
 
-        // Get all information for verified_contracts
-        const verifiedContract = contract;
+          // Get all information for verified_contracts
+          const verifiedContract = contract;
 
-        // Get all FK information
-        const {
-          rows: [deployment],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.contract_deployments WHERE id = $1`,
-          [deployment_id]
-        );
-        const {
-          rows: [deploymentContract],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.contracts WHERE id = $1`,
-          [deployment.contract_id]
-        );
-        const {
-          rows: [compilation],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.compiled_contracts WHERE id = $1`,
-          [compilation_id]
-        );
-        if (compilation.creation_code_hash === null) {
-          continue;
-        }
+          // Get all FK information
+          const {
+            rows: [deployment],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.contract_deployments WHERE id = $1`,
+            [deployment_id]
+          );
+          const {
+            rows: [deploymentContract],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.contracts WHERE id = $1`,
+            [deployment.contract_id]
+          );
+          const {
+            rows: [compilation],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.compiled_contracts WHERE id = $1`,
+            [compilation_id]
+          );
+          if (compilation.creation_code_hash === null) {
+            continue;
+          }
 
-        // Get creation and runtime code for the compilation
-        const {
-          rows: [compilationCreationCode],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
-          [compilation.creation_code_hash]
-        );
-        const {
-          rows: [compilationRuntimeCode],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
-          [compilation.runtime_code_hash]
-        );
+          // Get creation and runtime code for the compilation
+          const {
+            rows: [compilationCreationCode],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
+            [compilation.creation_code_hash]
+          );
+          const {
+            rows: [compilationRuntimeCode],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
+            [compilation.runtime_code_hash]
+          );
 
-        // Get creation and runtime code for the deployment
-        const {
-          rows: [deployedCreationCode],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
-          [deploymentContract.creation_code_hash]
-        );
-        const {
-          rows: [deployedRuntimeCode],
-        } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
-          [deploymentContract.runtime_code_hash]
-        );
+          // Get creation and runtime code for the deployment
+          const {
+            rows: [deployedCreationCode],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
+            [deploymentContract.creation_code_hash]
+          );
+          const {
+            rows: [deployedRuntimeCode],
+          } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.code WHERE code_hash = $1`,
+            [deploymentContract.runtime_code_hash]
+          );
 
-        // Insert dependencies into target DB and handle conflicts
+          // Insert dependencies into target DB and handle conflicts
 
-        // Insert creation and runtime code for the compilation into target DB and handle conflicts
-        await targetClient.query(
-          `
+          // Insert creation and runtime code for the compilation into target DB and handle conflicts
+          await targetClient.query(
+            `
             INSERT INTO ${VERA_SCHEMA}.code (code_hash, code, code_hash_keccak)
             VALUES ($1, $2, $3)
             ON CONFLICT (code_hash) DO NOTHING
           `,
-          [
-            compilationCreationCode.code_hash,
-            compilationCreationCode.code,
-            compilationCreationCode.code_hash_keccak,
-          ]
-        );
+            [
+              compilationCreationCode.code_hash,
+              compilationCreationCode.code,
+              compilationCreationCode.code_hash_keccak,
+            ]
+          );
 
-        await targetClient.query(
-          `
+          await targetClient.query(
+            `
             INSERT INTO ${VERA_SCHEMA}.code (code_hash, code, code_hash_keccak)
             VALUES ($1, $2, $3)
             ON CONFLICT (code_hash) DO NOTHING
           `,
-          [
-            compilationRuntimeCode.code_hash,
-            compilationRuntimeCode.code,
-            compilationRuntimeCode.code_hash_keccak,
-          ]
-        );
+            [
+              compilationRuntimeCode.code_hash,
+              compilationRuntimeCode.code,
+              compilationRuntimeCode.code_hash_keccak,
+            ]
+          );
 
-        // Insert creation and runtime code for the deployment into target DB and handle conflicts
-        await targetClient.query(
-          `
+          // Insert creation and runtime code for the deployment into target DB and handle conflicts
+          await targetClient.query(
+            `
             INSERT INTO ${VERA_SCHEMA}.code (code_hash, code, code_hash_keccak)
             VALUES ($1, $2, $3)
             ON CONFLICT (code_hash) DO NOTHING
           `,
-          [
-            deployedCreationCode.code_hash,
-            deployedCreationCode.code,
-            deployedCreationCode.code_hash_keccak,
-          ]
-        );
+            [
+              deployedCreationCode.code_hash,
+              deployedCreationCode.code,
+              deployedCreationCode.code_hash_keccak,
+            ]
+          );
 
-        await targetClient.query(
-          `
+          await targetClient.query(
+            `
             INSERT INTO ${VERA_SCHEMA}.code (code_hash, code, code_hash_keccak)
             VALUES ($1, $2, $3)
             ON CONFLICT (code_hash) DO NOTHING
           `,
-          [
-            deployedRuntimeCode.code_hash,
-            deployedRuntimeCode.code,
-            deployedRuntimeCode.code_hash_keccak,
-          ]
-        );
+            [
+              deployedRuntimeCode.code_hash,
+              deployedRuntimeCode.code,
+              deployedRuntimeCode.code_hash_keccak,
+            ]
+          );
 
-        const newContractIdValues = [
-          deploymentContract.creation_code_hash,
-          deploymentContract.runtime_code_hash,
-        ];
-        const newContractId = await upsertAndGetId(
-          `
+          const newContractIdValues = [
+            deploymentContract.creation_code_hash,
+            deploymentContract.runtime_code_hash,
+          ];
+          const newContractId = await upsertAndGetId(
+            `
             INSERT INTO ${VERA_SCHEMA}.contracts (creation_code_hash, runtime_code_hash)
             VALUES ($1, $2)
             ON CONFLICT (creation_code_hash, runtime_code_hash) DO NOTHING
           `,
-          `
+            `
             SELECT id FROM ${VERA_SCHEMA}.contracts WHERE creation_code_hash = $1 AND runtime_code_hash = $2
           `,
-          newContractIdValues,
-          newContractIdValues,
-          targetClient
-        );
+            newContractIdValues,
+            newContractIdValues,
+            targetClient
+          );
 
-        const newDeploymentIdValues = [
-          deployment.chain_id,
-          deployment.address,
-          deployment.transaction_hash,
-          deployment.block_number,
-          deployment.transaction_index,
-          deployment.deployer,
-          newContractId,
-        ];
-        const newDeploymentId = await upsertAndGetId(
-          `
+          const newDeploymentIdValues = [
+            deployment.chain_id,
+            deployment.address,
+            deployment.transaction_hash,
+            deployment.block_number,
+            deployment.transaction_index,
+            deployment.deployer,
+            newContractId,
+          ];
+          const newDeploymentId = await upsertAndGetId(
+            `
             INSERT INTO ${VERA_SCHEMA}.contract_deployments (chain_id, address, transaction_hash, block_number, transaction_index, deployer, contract_id)
             VALUES ($1, $2, $3, $4, $5, $6, $7)
             ON CONFLICT (chain_id, address, transaction_hash) DO NOTHING
           `,
-          `
+            `
             SELECT id FROM ${VERA_SCHEMA}.contract_deployments WHERE chain_id = $1 AND address = $2 AND transaction_hash = $3
           `,
-          newDeploymentIdValues,
-          newDeploymentIdValues.slice(0, 3),
-          targetClient
-        );
+            newDeploymentIdValues,
+            newDeploymentIdValues.slice(0, 3),
+            targetClient
+          );
 
-        const newCompilationIdValues = [
-          compilation.compiler,
-          compilation.language,
-          compilation.creation_code_hash,
-          compilation.runtime_code_hash,
-          compilation.version,
-          compilation.name,
-          compilation.fully_qualified_name,
-          compilation.compiler_settings,
-          compilation.compilation_artifacts,
-          compilation.creation_code_artifacts,
-          compilation.runtime_code_artifacts,
-        ];
-        const newCompilationId = await upsertAndGetId(
-          `
+          const newCompilationIdValues = [
+            compilation.compiler,
+            compilation.language,
+            compilation.creation_code_hash,
+            compilation.runtime_code_hash,
+            compilation.version,
+            compilation.name,
+            compilation.fully_qualified_name,
+            compilation.compiler_settings,
+            compilation.compilation_artifacts,
+            compilation.creation_code_artifacts,
+            compilation.runtime_code_artifacts,
+          ];
+          const newCompilationId = await upsertAndGetId(
+            `
             INSERT INTO ${VERA_SCHEMA}.compiled_contracts (
                 compiler, language, creation_code_hash, runtime_code_hash, version, name, fully_qualified_name, 
                 compiler_settings, compilation_artifacts, creation_code_artifacts, runtime_code_artifacts)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
             ON CONFLICT (compiler, language, creation_code_hash, runtime_code_hash) DO NOTHING
           `,
-          `
+            `
             SELECT id FROM ${VERA_SCHEMA}.compiled_contracts 
             WHERE compiler = $1 AND language = $2 AND creation_code_hash = $3 AND runtime_code_hash = $4
           `,
-          newCompilationIdValues,
-          newCompilationIdValues.slice(0, 4),
-          targetClient
-        );
-
-        // Get sources for the compilation
-        const { rows: compilationSources } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.compiled_contracts_sources WHERE compilation_id = $1`,
-          [compilation_id]
-        );
-
-        // Extract all source_hashes
-        const sourceHashes = compilationSources.map(
-          (source) => source.source_hash
-        );
-
-        // Fetch all relevant sources in a single query
-        const { rows: sourcesContent } = await sourceClient.query(
-          `SELECT * FROM ${SOURCIFY_SCHEMA}.sources WHERE source_hash = ANY($1)`,
-          [sourceHashes]
-        );
-
-        // Create a map for quick lookup
-        const sourceContentMap = new Map(
-          sourcesContent.map((source) => [
-            source.source_hash.toString("hex"),
-            source,
-          ])
-        );
-
-        // Insert sources and compiled_contracts_sources
-        for (const source of compilationSources) {
-          const sourceContent = sourceContentMap.get(
-            source.source_hash.toString("hex")
+            newCompilationIdValues,
+            newCompilationIdValues.slice(0, 4),
+            targetClient
           );
 
-          if (!sourceContent) {
-            console.warn(
-              `Source content not found for hash: ${source.source_hash.toString(
-                "hex"
-              )}`
-            );
-            continue;
-          }
+          // Get sources for the compilation
+          const { rows: compilationSources } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.compiled_contracts_sources WHERE compilation_id = $1`,
+            [compilation_id]
+          );
 
-          // Insert into sources table
-          const sourceValues = [
-            Buffer.from(sourceContent.source_hash),
-            Buffer.from(sourceContent.source_hash_keccak),
-            sourceContent.content,
-          ];
-          await targetClient.query(
-            `
+          // Extract all source_hashes
+          const sourceHashes = compilationSources.map(
+            (source) => source.source_hash
+          );
+
+          // Fetch all relevant sources in a single query
+          const { rows: sourcesContent } = await sourceClient.query(
+            `SELECT * FROM ${SOURCIFY_SCHEMA}.sources WHERE source_hash = ANY($1)`,
+            [sourceHashes]
+          );
+
+          // Create a map for quick lookup
+          const sourceContentMap = new Map(
+            sourcesContent.map((source) => [
+              source.source_hash.toString("hex"),
+              source,
+            ])
+          );
+
+          // Insert sources and compiled_contracts_sources
+          for (const source of compilationSources) {
+            const sourceContent = sourceContentMap.get(
+              source.source_hash.toString("hex")
+            );
+
+            if (!sourceContent) {
+              console.warn(
+                `Source content not found for hash: ${source.source_hash.toString(
+                  "hex"
+                )}`
+              );
+              continue;
+            }
+
+            // Insert into sources table
+            const sourceValues = [
+              Buffer.from(sourceContent.source_hash),
+              Buffer.from(sourceContent.source_hash_keccak),
+              sourceContent.content,
+            ];
+            await targetClient.query(
+              `
             INSERT INTO ${VERA_SCHEMA}.sources (source_hash, source_hash_keccak, content)
             VALUES ($1, $2, $3)
             ON CONFLICT (source_hash) DO NOTHING
             `,
-            sourceValues
-          );
+              sourceValues
+            );
 
-          // Insert into compiled_contracts_sources table
-          const compiledSourcesValues = [
-            newCompilationId,
-            Buffer.from(source.source_hash),
-            source.path,
-          ];
-          await targetClient.query(
-            `
+            // Insert into compiled_contracts_sources table
+            const compiledSourcesValues = [
+              newCompilationId,
+              Buffer.from(source.source_hash),
+              source.path,
+            ];
+            await targetClient.query(
+              `
             INSERT INTO ${VERA_SCHEMA}.compiled_contracts_sources (compilation_id, source_hash, path)
             VALUES ($1, $2, $3)
             ON CONFLICT (compilation_id, path) DO NOTHING
             `,
-            compiledSourcesValues
-          );
-        }
+              compiledSourcesValues
+            );
+          }
 
-        await targetClient.query(
-          `
+          await targetClient.query(
+            `
             INSERT INTO ${VERA_SCHEMA}.verified_contracts (
                 created_at, updated_at, created_by, updated_by, deployment_id, compilation_id, 
                 creation_match, creation_values, creation_transformations, creation_metadata_match,
@@ -388,23 +389,26 @@ async function upsertAndGetId(
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
             ON CONFLICT (compilation_id, deployment_id) DO NOTHING
           `,
-          [
-            verifiedContract.created_at,
-            verifiedContract.updated_at,
-            verifiedContract.created_by,
-            verifiedContract.updated_by,
-            newDeploymentId,
-            newCompilationId,
-            verifiedContract.creation_match,
-            verifiedContract.creation_values,
-            JSON.stringify(verifiedContract.creation_transformations),
-            verifiedContract.creation_metadata_match,
-            verifiedContract.runtime_match,
-            verifiedContract.runtime_values,
-            JSON.stringify(verifiedContract.runtime_transformations),
-            verifiedContract.runtime_metadata_match,
-          ]
-        );
+            [
+              verifiedContract.created_at,
+              verifiedContract.updated_at,
+              verifiedContract.created_by,
+              verifiedContract.updated_by,
+              newDeploymentId,
+              newCompilationId,
+              verifiedContract.creation_match,
+              verifiedContract.creation_values,
+              JSON.stringify(verifiedContract.creation_transformations),
+              verifiedContract.creation_metadata_match,
+              verifiedContract.runtime_match,
+              verifiedContract.runtime_values,
+              JSON.stringify(verifiedContract.runtime_transformations),
+              verifiedContract.runtime_metadata_match,
+            ]
+          );
+        } catch (error) {
+          console.error("Error processing contract:", error);
+        }
       }
 
       const endIterationTime = performance.now();
